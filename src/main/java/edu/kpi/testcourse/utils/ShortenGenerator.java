803350f03a3e4574ba064660f6c98829eacc1ff2
quppeq <quppeq@gmail.com>
2021-03-09 20:14:26 +0200

    create empte shortenGenerator with length test

---
+++ src/main/java/edu/kpi/testcourse/utils/ShortenGenerator.java
@@ -0,0 +1,8 @@
+package edu.kpi.testcourse.utils;
+
+public class ShortenGenerator {
+
+  public String generate(int i) {
+    return "NotImplemented";
+  }
+}
---
+++ src/test/java/edu/kpi/testcourse/utils/ShortenGeneratorTest.java
@@ -0,0 +1,18 @@
+package edu.kpi.testcourse.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ShortenGeneratorTest {
+
+  @Test
+  void checkShortenLen(){
+    ShortenGenerator shortenGenerator = new ShortenGenerator();
+
+    String shorten = shortenGenerator.generate(17);
+
+    assertThat(shorten.length()).isEqualTo(17);
+  }
+
+}
