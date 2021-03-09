b13429bca5440fcc06209c587e54ba59d4a73260
unknown <suhonos.p@gmail.com>
2021-03-09 22:10:06 +0200

    Added random string generator and tests

--- src/main/java/edu/kpi/testcourse/utils/ShortenGenerator.java
+++ src/main/java/edu/kpi/testcourse/utils/ShortenGenerator.java
@@ -2,7 +2,54 @@ package edu.kpi.testcourse.utils;
 
 public class ShortenGenerator {
 
-  public String generate(int i) {
-    return "NotImplemented";
+  // function to generate a random string of length n
+  static String generate(int n) {
+
+      // chose a Character random from this String
+      String AlphaNumericString = "abcdefghijklmnopqrstuvxyz";
+
+      // create StringBuffer size of AlphaNumericString
+      StringBuilder sb = new StringBuilder(n);
+
+      for (int i = 0; i < n; i++) {
+
+        // generate a random number between
+        // 0 to AlphaNumericString variable length
+        int index
+          = (int)(AlphaNumericString.length()
+          * Math.random());
+
+        // add Character one by one in end of sb
+        sb.append(AlphaNumericString
+          .charAt(index));
+      }
+
+      return sb.toString();
   }
+
+  static String generate() {
+
+    final int n = 8;
+    // chose a Character random from this String
+    String AlphaNumericString = "abcdefghijklmnopqrstuvxyz";
+
+    // create StringBuffer size of AlphaNumericString
+    StringBuilder sb = new StringBuilder(n);
+
+    for (int i = 0; i < n; i++) {
+
+      // generate a random number between
+      // 0 to AlphaNumericString variable length
+      int index
+        = (int)(AlphaNumericString.length()
+        * Math.random());
+
+      // add Character one by one in end of sb
+      sb.append(AlphaNumericString
+        .charAt(index));
+    }
+
+    return sb.toString();
+  }
+
 }
--- src/test/java/edu/kpi/testcourse/utils/ShortenGeneratorTest.java
+++ src/test/java/edu/kpi/testcourse/utils/ShortenGeneratorTest.java
@@ -15,4 +15,13 @@ public class ShortenGeneratorTest {
     assertThat(shorten.length()).isEqualTo(17);
   }
 
+  @Test
+  void checkEmptyN(){
+    ShortenGenerator shortenGenerator = new ShortenGenerator();
+
+    String shorten = shortenGenerator.generate();
+
+    assertThat(shorten.length()).isEqualTo(8);
+  }
+
 }
