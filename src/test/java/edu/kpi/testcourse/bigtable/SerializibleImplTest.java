eabae5ebfae5395fc18e9c485eac00d59815fa36
quppeq <quppeq@gmail.com>
2021-03-11 01:07:21 +0200

    add serialize tests

---
+++ src/test/java/edu/kpi/testcourse/bigtable/SerializibleImplTest.java
@@ -0,0 +1,49 @@
+package edu.kpi.testcourse.bigtable;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SerializibleImplTest {
+
+  @Test
+  void checkUserToJsonSerialize(){
+    UserDaoImpl dao = new UserDaoImpl();
+    dao.put("test@mail.com", "hash");
+
+    String json = dao.toJson();
+
+    assertThat(json).isEqualTo("{\"test@mail.com\":\"hash\"}");
+    assertThat(json).isNotEqualTo("{\"test@mail.com\":\"hashh\"}");
+  }
+
+  @Test
+  void checkTokenToJsonSerialize(){
+    TokenDaoImpl dao = new TokenDaoImpl();
+    dao.add("test@mail.com", "asd");
+
+    String json = dao.toJson();
+
+    assertThat(json).isEqualTo("{\"test@mail.com\":[\"asd\"]}");
+    assertThat(json).isNotEqualTo("{\"test@mail.com\":[\"asdd\"]}");
+  }
+
+  @Test
+  void checkAliasToJsonSerialize(){
+    AliasDaoImpl dao = new AliasDaoImpl();
+    String shrt = "shrt";
+    Alias alias = new Alias(shrt, "longUrl", "itsMeMario");
+    dao.add(shrt, alias);
+
+    String json = dao.toJson();
+
+    assertThat(json).isEqualTo("{\"shrt\":"
+      + "{\"shorten\":\"shrt\","
+      + "\"url\":\"longUrl\","
+      + "\"username\":\"itsMeMario\"}}");
+    assertThat(json).isNotEqualTo("{\"shrt\":"
+      + "{\"shorten\":\"shrt\","
+      + "\"url\":\"longUrl\","
+      + "\"username\":\"itsMeWario\"}}");
+  }
+
+}
