55b42537b9b04beabf274932c000a139e8eceb12
unknown <suhonos.p@gmail.com>
2021-03-11 21:30:21 +0200

    Added tests for serialization, fromJson isn`t working properly

--- src/test/java/edu/kpi/testcourse/bigtable/SerializibleImplTest.java
+++ src/test/java/edu/kpi/testcourse/bigtable/SerializibleImplTest.java
@@ -1,12 +1,13 @@
 package edu.kpi.testcourse.bigtable;
 
-import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 class SerializibleImplTest {
 
   @Test
-  void checkUserToJsonSerialize(){
+  void checkUserToJsonSerialize() {
     UserDaoImpl dao = new UserDaoImpl();
     dao.put("test@mail.com", "hash");
 
@@ -19,7 +20,14 @@ class SerializibleImplTest {
   }
 
   @Test
-  void checkTokenToJsonSerialize(){
+  void checkJsonToUserDeserialize() {
+    UserDaoImpl userdao = new UserDaoImpl();
+    userdao.fromJson("{test@mail.com:hash}");
+    assertThat(userdao.get("test@mail.com")).isEqualTo("hash");
+  }
+
+  @Test
+  void checkTokenToJsonSerialize() {
     TokenDaoImpl dao = new TokenDaoImpl();
     dao.add("test@mail.com", "asd");
 
@@ -32,18 +40,29 @@ class SerializibleImplTest {
   }
 
   @Test
-  void checkAliasToJsonSerialize(){
+  void checkAliasToJsonSerialize() {
     AliasDaoImpl dao = new AliasDaoImpl();
     String shrt = "shrt";
     Alias alias = new Alias(shrt, "longUrl", "itsMeMario");
     dao.add(shrt, alias);
-
     String json = dao.toJson();
 
     assertThat(json).isEqualTo("""
-       {"shrt":{"shorten":"shrt","url":"longUrl","username":"itsMeMario"}}""");
+      {"shrt":{"shorten":"shrt","url":"longUrl","username":"itsMeMario"}}""");
     assertThat(json).isNotEqualTo("""
-       {"shrt":{"shorten":"shrt","url":"longUrl","username":"itsMeWario"}}""");
+      {"shrt":{"shorten":"shrt","url":"longUrl","username":"itsMeWario"}}""");
+  }
+
+  @Test
+  void checkJsonToAliasDeserialize() {
+    AliasDaoImpl dao = new AliasDaoImpl();
+    dao.fromJson("""
+      {"shrt":{"shorten":"shrt","url":"longUrl","username":"itsMeMario"}}""");
+    String shrt = "shrt";
+    Alias etalon = new Alias(shrt, "longUrl", "itsMeMario");
+    Alias etalon2 = dao.get(shrt);
+    assertThat(etalon2).isEqualTo(etalon);
+
   }
 
 }
