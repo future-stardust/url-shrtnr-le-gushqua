b320381468ab5ad272de3726eb84b73ff7550968
unknown <suhonos.p@gmail.com>
2021-03-11 19:55:20 +0200

    Fixed checkstyle for passing tests

--- src/main/java/edu/kpi/testcourse/rest/ApiController.java
+++ src/main/java/edu/kpi/testcourse/rest/ApiController.java
@@ -4,9 +4,9 @@ import edu.kpi.testcourse.Main;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
-import io.micronaut.http.annotation.Delete;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import javax.inject.Inject;
@@ -26,47 +26,47 @@ public class ApiController {
   }
 
   /**
-   * Create an URL alias
+   * Create an URL alias.
    *
-   * @param url: string, required - link which has to be shortened,
-   * @param alias: string, optional - desired alias for a full link
+   * @param url string, required - link which has to be shortened.
+   * @param alias string, optional - desired alias for a full link.
    *
-   * @return OK/error
+   * @return OK/error.
    */
   @Post(value = "/urls/shorten", consumes = MediaType.APPLICATION_JSON)
-  public HttpResponse<Object> shortenUrl(String url, String alias){
+  public HttpResponse<Object> shortenUrl(String url, String alias) {
     return HttpResponse.ok();
   }
 
   /**
-   * List of user's aliases
+   * List of user's aliases.
    *
-   * @return array of user's aliases
+   * @return array of user's aliases.
    */
   @Get(value = "/urls", produces = MediaType.APPLICATION_JSON)
-  public String[] getUserAliases(){
+  public String[] getUserAliases() {
     return new String[]{"Some aliases"};
   }
 
   /**
-   * Delete alias
+   * Delete alias.
    *
-   * @param alias: string, required - alias, that needs to be removed
-   * @return OK/error
+   * @param alias string, required - alias, that needs to be removed.
+   * @return OK/error.
    */
   @Delete(value = "/urls/<alias>")
-  public HttpResponse<Object> deleteAlias(String alias){
+  public HttpResponse<Object> deleteAlias(String alias) {
     return HttpResponse.ok();
   }
 
   /**
-   * Redirect by alias to a full link
+   * Redirect by alias to a full link.
    *
-   * @param alias: string, required - alias for the full link
-   * @return Redirect/Error
+   * @param alias string, required - alias for the full link.
+   * @return Redirect/Error.
    */
   @Get(value = "/r/<alias>", produces = MediaType.APPLICATION_JSON)
-  public HttpResponse<Object> redirectToUrl(String alias){
+  public HttpResponse<Object> redirectToUrl(String alias) {
     return HttpResponse.ok();
   }
 }
