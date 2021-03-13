2f26ef5f31bb1585b6881d068cddb1599897a66e
Tatyana Gladka <tgladka@promptlink.com>
2021-03-13 15:15:53 +0200

    use single controller for auth

--- src/main/java/edu/kpi/testcourse/rest/AuthController.java
+++ src/main/java/edu/kpi/testcourse/rest/AuthController.java
@@ -1,33 +1,40 @@
 package edu.kpi.testcourse.rest;
 
+import edu.kpi.testcourse.auth.JwtUtils;
 import edu.kpi.testcourse.auth.Pbkdf2PasswordEncoder;
 import edu.kpi.testcourse.auth.UserCredentials;
+import edu.kpi.testcourse.bigtable.TokenDao;
 import edu.kpi.testcourse.bigtable.UserDao;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.hateoas.JsonError;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import java.text.ParseException;
 import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * REST API controller that provides logic for user registration.
+ * REST API controller that provides logic for user authorization.
  */
-@Secured(SecurityRule.IS_ANONYMOUS)
 @Produces
 @Controller
-public class RegistrationController {
+public class AuthController {
 
-  private static final Logger logger = LoggerFactory.getLogger(RegistrationController.class);
+  private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
   @Inject
   private UserDao userDao;
+
+  @Inject
+  private TokenDao tokenDao;
 
   /**
    * Registers user.
@@ -35,6 +42,7 @@ public class RegistrationController {
    * @param credentials user email and password
    * @return http response
    */
+  @Secured(SecurityRule.IS_ANONYMOUS)
   @Post(value = "/signup")
   public MutableHttpResponse<JsonError> signUp(@Body UserCredentials credentials) {
     logger.info("Sign up");
@@ -44,6 +52,25 @@ public class RegistrationController {
     }
     String encodedPassword = Pbkdf2PasswordEncoder.encodePassword(credentials.getPassword());
     userDao.put(credentials.getUsername(), encodedPassword);
+
+    return HttpResponse.ok();
+  }
+
+  /**
+   * Logouts user.
+   *
+   * @param authorization user JWT token
+   * @return http response
+   */
+  @Secured(SecurityRule.IS_AUTHENTICATED)
+  @Get(value = "/signout")
+  public MutableHttpResponse<JsonError> signOut(@Header String authorization)
+    throws ParseException {
+    logger.info("Sign out");
+
+    String token = JwtUtils.getToken(authorization);
+    String email = JwtUtils.getUsername(token);
+    tokenDao.add(email, token);
 
     return HttpResponse.ok();
   }
